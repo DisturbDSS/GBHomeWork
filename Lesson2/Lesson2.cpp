5d67ce7089ba438e84d1e136c0a64e8d8c4c4a33
Bogdan <bogdan_dss@mail.ru>
2021-09-11 19:07:27 +0200

    TaskChanged

--- Lesson2/Lesson2.cpp
+++ Lesson2/Lesson2.cpp
@@ -1,20 +1,53 @@
-﻿// Lesson2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "HomeWork2" << std::endl;
+
+    //task1
+    {
+        bool flag{ false };
+        char chr = 'A';
+        short int var1 = -32768;
+        int var2 = 100;
+        long long var3 = 9999999999999999999;
+        float var4 = 7.77f;
+        double var5 = 7.1E-5;
+        std::cout << chr << sizeof(chr) << std::endl;
+        std::cout << var1 << " size is:" << sizeof(var1) << std::endl;
+    };
+    //task2
+    {
+        enum XO { CROSS = 'x', ZERO = 'o', NONE = 'n' };
+
+        //task3
+        const unsigned int FieldSize = 3;
+        XO array[FieldSize][FieldSize] = { {NONE, NONE, NONE}, {NONE, NONE, NONE}, {NONE, NONE, NONE} };
+
+        std::cout << array[0][0] << ' ' << array[0][1] << ' ' << array[0][2] << std::endl;
+        std::cout << array[1][0] << ' ' << array[1][1] << ' ' << array[1][2] << std::endl;
+        std::cout << array[2][0] << ' ' << array[2][1] << ' ' << array[2][2] << std::endl;
+
+
+
+        //task4
+
+        struct XOStruct
+        {
+
+            XO array[FieldSize][FieldSize];
+            bool is_game_end;
+            bool is_player1_won;
+            long long Player1ID, player2ID;
+            XO cPlayer1, cPlayer2;
+
+
+        };
+
+    };
+
+    //task5
+
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
