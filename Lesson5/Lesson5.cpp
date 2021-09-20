d986fde5e2698058149d0913be22bf6b64a0c4a5
Bogdan <bogdan_dss@mail.ru>
2021-09-20 02:24:50 +0200

    lesson5

---
+++ Lesson5/Lesson5.cpp
@@ -0,0 +1,112 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+void printArr(double arr[], const size_t size) ////task1function
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "Array is printed" << endl;
+    
+}
+
+void printOut(int* arr, const int size) ////task2function
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void Replacer(int* arr, const int size) ////task2function
+{
+    for (int i = 0; i < size; i++)
+        if (arr[i]) arr[i] = 0;
+        else arr[i] = 1;
+
+}
+
+void initArr(int* arr, int size) ////task3function
+{
+    int arr0[8] = { 1, 4, 7, 10, 13, 16, 19, 22 };
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = arr0[i];
+    }
+}
+
+void shift(int* arr, int size, int nShift) ////task4function
+{
+    int index;
+    int arr0[8];
+    for (int i = 0; i < size; i++)    
+    {
+        index = i + nShift - size;
+        
+        if ((i + nShift) >= size)
+            index = i + nShift - size;
+        else index = i + nShift;
+        arr0[index] = arr[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = arr0[i];
+    }
+}
+
+
+int main()
+{
+    {   //task1
+        const int arrsize = 5;
+        double arr1[] = { 3.2, 5, 6, 8, 10 };
+        printArr(arr1, arrsize);
+    }
+
+    {
+        //task2
+        const int arrsize = 9;
+        int arr2[] = { 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+
+        printOut(arr2, arrsize);
+        Replacer(arr2, arrsize);
+        printOut(arr2, arrsize);
+    }
+
+
+    {
+        //task3
+        const int arrsize = 8;
+        int arr3[arrsize];
+        initArr(arr3, arrsize);
+        printOut(arr3, arrsize);
+
+    }
+
+
+    {
+        //task4
+        int nShift;
+        cout << " Enter number offset 0-8: ";
+        cin >> nShift;
+        const int arrsize = 8;
+        int origin[arrsize] = { 1, 4, 7, 10, 13, 16, 19, 22 };
+
+        if (nShift <= arrsize)
+        {
+            shift(origin, arrsize, nShift);
+            printOut(origin, arrsize);
+        }
+
+        else
+            cout << "Number is out of range!";
+
+    }
+
+
+
+    return 0;
+}
